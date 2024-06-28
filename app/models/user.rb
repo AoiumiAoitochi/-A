b8779d90d53d65a6b0516940ede4012f084afdb7
Aoi <hoyagaoishii@gmail.com>
2024-06-28 15:25:48 +0900

    add validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 } #presence（プレゼンス）はオブジェクトが空または,nilでないか検証
+  validates :email, presence: true, length: { maximum: 100 } #バリデーツ・プレゼンス・トゥルー・レングス・コロン・マキシマム
+                  #lenght　文字数最低3文字,maximum 最大１２０文字
+
 end
