d9c7d07a2a16dfc7523e2e46b1306aef1ecb16c8
Aoi <hoyagaoishii@gmail.com>
2024-07-04 20:27:01 +0900

    passwordハッシュ設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 50 } #presence（プレゼンス）はオブジェクトが空または,nilでないか検証
-  validates :email, presence: true, length: { maximum: 100 } #バリデーツ・プレゼンス・トゥルー・レングス・コロン・マキシマム
-                  #lenght　文字数最低3文字,maximum 最大１２０文字
+  before_save {self.email = email.downcse}
 
+  validates :name, presence: true, length: { maximum: 50 } #presence（プレゼンス）はオブジェクトが空または,nilでないか検証
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 100 },
+                                    format: { with: VALID_EMAIL_REGEX },
+                                    uniqueness: true
+                  #バリデーツ・プレゼンス・トゥルー・レングス・コロン・マキシマム
+                  #lenght　文字数最低3文字,maximum 最大１２０文字
+  has_secure_password #passwordをハッシュ化（入力されたデータ（パスワード）を元に戻せないデータにする処理）
+  validates :password,presence: true,lenght: {minimum: 6}
 end
