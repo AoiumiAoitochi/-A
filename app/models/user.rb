eb049acd4ae11e9c5e8c4376da8871cc2b52dfca
Aoi <hoyagaoishii@gmail.com>
2024-07-05 10:31:19 +0900

    validates add

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  before_save {self.email = email.downcse}
+  before_save {self.email = email.downcase}
 
   validates :name, presence: true, length: { maximum: 50 } #presence（プレゼンス）はオブジェクトが空または,nilでないか検証
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -9,5 +9,5 @@ class User < ApplicationRecord
                   #バリデーツ・プレゼンス・トゥルー・レングス・コロン・マキシマム
                   #lenght　文字数最低3文字,maximum 最大１２０文字
   has_secure_password #passwordをハッシュ化（入力されたデータ（パスワード）を元に戻せないデータにする処理）
-  validates :password,presence: true,lenght: {minimum: 6}
+  validates :password,presence: true,length: {minimum: 6}
 end
